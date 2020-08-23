9fc285c5e7c82c91091f3996869c3b084be117e3
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 15:30:21 -0500

    agregando variables

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
         <h1>
             <?php
             echo 'Hola mundo!!';
+
+            $var1 = 'texto ';
+            $var2 = 10;
+            $var3 = 5;
+            echo $var1 . $var2;
+            echo 'suma ' . $var2 + $var3;
             ?>
         </h1>
     </body>
