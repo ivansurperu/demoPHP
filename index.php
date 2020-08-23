359c605694457c0c5968ab9ba0fadfe31f443934
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 16:08:29 -0500

    ajuste de operaciones

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
         <h1>Esto es PHP!!!</h1>
         <h1>
             <?php
-            echo 'Hola mundo!!';\n\r;
+            echo 'Hola mundo!!';
 
-            $var1 = 'texto ';
+            $var1 = "texto ";
             $var2 = 10;
             $var3 = 4;
 
-            echo $var1 . $var2;\n\r;
+            echo $var1 . $var2;
             
-            echo "suma: " .  $var2 + $var3;\n\r;
-            echo "resta: " . $var2 - $var3;\n\r;
-            echo "multiplica: " . $var2 * $var3;\n\r;
-            echo "divide: " . $var2 / $var3;\n\r;
+            echo "suma: " .  $var2 + $var3;
+            echo "resta: " . $var2 - $var3;
+            echo "multiplica: " . $var2 * $var3;
+            echo "divide: " . $var2 / $var3;
             ?>
         </h1>
     </body>
