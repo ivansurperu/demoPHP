2c516353333d1e1e90f293da60c4ae9077fcc6fc
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 18:31:09 -0500

    practica 1

---
+++ practica1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Demo 1
+        </title>        
+    </head>
+    <body>
+        <h1>1. VARIBLES</h1>
+        <?php
+        //declarar variables
+        $nombre="Ivan";
+        $edad=18;
+
+        echo "el nombre es: ".$nombre;
+        
+        ?>
+
+    </body>
+</html>
+
