cc139a27ba2149c74583452a6ac21f1a0ac6218e
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 14:37:47 -0500

    creando variables

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplos de PHP</title>
+    </head>
+    <body>
+        <h1>Esto es PHP!!!</h1>
+        <h1>
+            <?php
+            echo 'Hola mundo!!';
+            $var1 = 123;
+            $var2 = 'texto';
+            echo $var2 . $var1 
+            
+            ?>
+
+
+        </h1>
+    </body>
+</html>
