493ea642b4a4a842a0a121cc957b34381c82697b
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 20:03:40 -0500

    practica3

--- practica3.php
+++ practica3.php
@@ -16,7 +16,31 @@
         }
          
         calculo1();
+        echo "<br>";
         
+        //funciones con parametros;
+        $valorA=2;
+        $valorB=3;
+        $valorC=4;
+
+        function operaciones(){
+            echo "variable A ". $valorA."<br>";    
+            echo "variable B ". $valorB."<br>";   
+            echo "variable C ". $valorC."<br>";    
+            echo "el valor de la funcion Suma (C + B) es ".functionSuma($valorC,$valorB);
+            echo "<br>";
+            echo "el valor de la funcion Resta (C - A) es ".functionResta($valorC,$valorA);
+
+        }
+         functionSuma($val1,$val2){
+            echo $val1 + $val2;
+         }
+         functionResta($val1,$val2){
+            echo $val1 - $val2;
+         }
+
+         operaciones();
+
         ?>
 
     </body>
