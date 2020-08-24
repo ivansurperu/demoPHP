1a50478d68990e286a0e14027755c4f0fd798786
Ivan Aguirre <ivansurperu@gmail.com>
2020-08-23 19:07:17 -0500

    practica 2

---
+++ practica2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Demo 2
+        </title>        
+    </head>
+    <body>
+        <h1>2. VARIBLES</h1>
+        <?php
+        //declarar variables
+        $entero=1234;
+        $real=3.141516;
+        $cadena="Hola Amigo";
+        $verdadero=true;
+        $falso=false;
+
+        echo "entero ".$entero;
+        echo "real ".$real;
+        echo "cadena ".$cadena;
+        echo "verdadero ".$verdadero;
+        echo "falso ".$falso;
+
+
+        //tipado dimamico
+        $cadena1="5";
+        $numero1=3;
+        echo $cadena1+$numero1;
+
+        ?>
+
+    </body>
+</html>
+
